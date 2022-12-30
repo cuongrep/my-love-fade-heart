043800f002e46d8f0d8988bef413f2146293b921
Cuong Le <cuongrep@gmail.com>
2022-12-31 00:16:05 +0900

    add static html file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,29 @@
-import * as React from "react"
+import { navigate } from "gatsby"
+import React, { useEffect } from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const iframeStyle = {
+    "height": "calc(100vh - 4px)",
+    "width": "calc(100vw - 4px)",
+    "overflow": "hidden",
+    "box-sizing": "border-box",
+  }
+  
+  useEffect(() => {
+    navigate('/fadeHeart.html');
+  }, [])
+
+  return (
+    <></>
+    // <>
+    //   {navigate()}
+    // </>
+    // <div
+    //   style={{
+    //     display: "flex",
+    //     alignContent: "center"
+    //   }}>
+    //   <iframe src="/fadeHeart.html" title="Fade Heart" allowfullscreen></iframe>
+    // </div>
+  )
 }
